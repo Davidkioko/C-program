a6dde681ef6225c9c7dd71a73da3d276e6c20505
Davidkioko <diegosantos13802@gmail.com>
2021-02-05 20:51:41 +0300

    First Comment

---
+++ speed1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#define SPEED 55 // you can also you for string constants #define GIRLFRIEND “Brenda” 
+int main() 
+{ 
+printf("Now, the speed limit here is %i.\n",SPEED); 
+printf("But I clocked you doin’ %i.\n",SPEED+15); 
+printf("Didn’t you see that %i MPH sign?\n",SPEED); 
+return(0); 
+}
